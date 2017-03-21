b77c47088b375f749d3138561a1a798fd1854272
Philipp <PhilSantay@gmail.com>
2017-03-21 10:27:25 +0100

    Added all components

---
+++ src/app/content/content.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: 'content.component.html',
+  styles: [`
+    .container{
+      display: block;
+      float: left;
+      margin-left: 1rem;
+    }
+  `]
+})
+
+export class ContentComponent{
+
+}
